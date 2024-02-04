012b9ff197ea0118b5812652df4f0bb0a9011eb6
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-02-04 11:50:03 +0530

    error correction

--- dummy_university/frontend/public/Home.js
+++ dummy_university/frontend/public/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
   return (
-    <iframe src="/home.html" width="100%" height="500"></iframe>
+    <iframe src="/home.html" width="100%" height="500" title="Home Page Content"></iframe>
   );
 }
 
