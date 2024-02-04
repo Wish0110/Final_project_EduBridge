9c467c8c69e68d37066ed7efe0be58afdf2f8ea2
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-02-04 19:25:42 +0530

    interface

---
+++ dummy_university/frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>NSBM Student Portal</h1>
+        <nav>
+          <ul>
+            <li><a href="#">University of Plymouth</a></li>
+            <li><a href="#">Plymouth University</a></li>
+            <li><a href="#">Bard Victoria University</a></li>
+            <li><a href="#">Melbourne Australia Victoria University</a></li>
+            <li><a href="#">USAD Test</a></li>
+          </ul>
+        </nav>
+        <main>
+          <h2>Online Services for Students</h2>
+          <ul>
+            <li><a href="#">Learning Management Systems (LMS) & Examinations</a></li>
+          </ul>
+        </main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
