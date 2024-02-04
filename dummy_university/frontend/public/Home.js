3e1f2a4dd99a7f83b503a60c093ca1899203b596
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-02-04 12:03:10 +0530

    dlt unnessasry

--- dummy_university/frontend/public/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Home() {
-  return (
-    <iframe src="/home.html" width="100%" height="500" title="Home Page Content"></iframe>
-  );
-}
-
-export default Home;
