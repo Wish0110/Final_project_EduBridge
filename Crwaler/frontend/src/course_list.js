5257710f13054f09262b47a786eccdc9ddb58d8a
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-02-22 13:35:26 +0530

    chngs

--- Crwaler/frontend/src/course_list.js
+++ Crwaler/frontend/src/course_list.js
@@ -40,38 +40,6 @@ function App() {
   return (
     <div>
       <h2>{data.courseTitle}</h2>
-      <h3>{data.schoolTitle}</h3>
-      <p>{data.overviewText}</p>
-      <h4>{data.careerTopic}</h4>
-      <p>{data.careerDescript}</p>
-      <h4>{data.keyFeaturesTopic}</h4>
-      <ul>
-        {data.keyFeaturesList.map((feature, index) => (
-          <li key={index}>{feature}</li>
-        ))}
-      </ul>
-      <h4>{data.courseMain}</h4>
-      <ul>
-        {data.ulElement3.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      <h4>{data.entryreqTopic}</h4>
-      <p>{data.entryreq}</p>
-      <p>{data.entryreqdetails}</p>
-      <h4>{data.feesTopic}</h4>
-      <p>{data.feesDetails}</p>
-      <p>{data.feesDetailsextra}</p>
-      <p>{data.feesDetailsadd}</p>
-      <p>{data.feesDetailsaddDetails}</p>
-      <h4>{data.applytopic}</h4>
-      <p>{data.applydetails}</p>
-      <h4>{data.careerTopic}</h4>
-      <ul>
-        {data.careers.map((career, index) => (
-          <li key={index}>{career}</li>
-        ))}
-      </ul>
     </div>
   );
 }
