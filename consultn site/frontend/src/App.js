ee09bd831dd029861a0eafece90196c19a4d328b
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-05 13:59:49 +0530

    chngs

--- consultn site/frontend/src/App.js
+++ consultn site/frontend/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/home';
-import About from './pages/about';
-import Service from './pages/service';
-import Help from './pages/help';
+import { BrowserRouter as Router } from 'react-router-dom';
+/*import Home from '';
+import About from '';
+import Service from '';
+import Help from '';*/
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
+      {/*<Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/service" component={Service} />
+        <Route path="/" component={About} />
+        <Route path="/" component={Service} />
         <Route path="/help" component={Help} />
-      </Switch>
+      </Switch>*/}
     </Router>
   );
 }
