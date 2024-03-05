a553d8913990c2fbea3b6a972476ee86c6160c2c
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-05 14:01:34 +0530

    nv br

--- consultn site/frontend/src/components/navbar.js
+++ consultn site/frontend/src/components/navbar.js
@@ -1,48 +1,117 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './navbar.css';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Container } from "@mui/material";
 
-const Navbar = () => {
-  const [scroll, setScroll] = useState(false);
-
-  const handleScroll = () => {
-    if (window.scrollY > 10) {
-      setScroll(true);
-    } else {
-      setScroll(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+function MyComponent() {
   return (
-    <div className={`navbar ${scroll ? 'scroll' : ''}`}>
-      <div className="navbar-left">
-        <Link to="/">
-          <img src="/logo.png" alt="Logo" className="navbar-logo" />
-        </Link>
-      </div>
-      <div className="navbar-right">
-        <Link to="/home" className="navbar-button">
-          Home
-        </Link>
-        <Link to="/about" className="navbar-button">
-          About
-        </Link>
-        <Link to="/service" className="navbar-button">
-          Service
-        </Link>
-        <Link to="/help" className="navbar-button">
-          Help
-        </Link>
-      </div>
-    </div>
-  );
-};
+    <Container maxWidth="xl">
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "#e6c235",
+          color: "#000000",
+          width: "108%",
+          transform: "translateX(-4%)",
+        }}
+      >
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+            }}
+          >
+            <b>DWANP</b> restaurent
+          </Typography>
+          <Button
+            href="/Home"
+            sx={{
+              color: "black",
+              marginRight: "1rem",
+              "&:hover": {
+                backgroundColor: "black",
+                color: "white",
+              },
+            }}
+          >
+            <b>Home</b>
+          </Button>
+          <Button
 
-export default Navbar;
+            href="/Employee"
+            sx={{
+              color: "black",
+              marginRight: "1rem",
+              "&:hover": {
+                backgroundColor: "black",
+                color: "white",
+              },
+            }}
+          >
+            <b>Employee</b>
+          </Button>
+          <Button
+            href="/Inventory"
+            sx={{
+              color: "black",
+              marginRight: "1rem",
+              "&:hover": {
+                backgroundColor: "black",
+                color: "white",
+              },
+            }}
+          >
+            <b>Inventory</b>
+          </Button>
+          <Button
+            
+            href="/Menu"
+            sx={{
+              color: "black",
+              marginRight: "1rem",
+              "&:hover": {
+                backgroundColor: "black",
+                color: "white",
+              },
+            }}
+          >
+            <b>Menu</b>
+          </Button>
+          <Button
+            
+            href="/Order"
+            sx={{
+              color: "black",
+              marginRight: "1rem",
+              "&:hover": {
+                backgroundColor: "black",
+                color: "white",
+              },
+            }}
+          >
+            <b>Order</b>
+          </Button>
+          <Button
+            
+            href="/Table"
+            sx={{
+              color: "black",
+              marginRight: "1rem",
+              "&:hover": {
+                backgroundColor: "black",
+                color: "white",
+              },
+            }}
+          >
+            <b>Table</b>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Container>
+  );
+}
+
+export default MyComponent;
