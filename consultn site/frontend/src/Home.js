7060c14e7222ef0302820b5e380cb75c55c309a4
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-05 20:18:42 +0530

    navigation

--- consultn site/frontend/src/Home.js
+++ consultn site/frontend/src/Home.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Map from './map';
 import banner from './banner.png';
 import Navbar from './navbar';
 import './App.css';
 
 const Home = () => {
-    return (
-        <div className="home">
-            <img src={banner} alt="Home Page Banner" className="banner" />
-            <Navbar />
-            <div>
-                <Routes>
-                    <Route path="/map" element={<Map />} />
-                </Routes>
-            </div>
-        </div>
-    );
+    const navigate = useNavigate();
+  return (
+    <div className="home">
+      <img src={banner} alt="Home Page Banner" className="banner" />
+      <Navbar />
+      <Routes>
+        <Route path="/map" element={<Map />} />
+        <Route exact path="/" element={<div>
+
+          <button className="map-button" onClick={() => navigate('/map')}>
+            Map
+          </button>
+        </div>} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Home;
