506a9b307443688f44a6e1600e72934da6acc4a4
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-05 19:33:56 +0530

    new

--- consultn site/frontend/src/App.js
+++ consultn site/frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Navbar from './navbar';
+
 import Home from './Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Home />
     </div>
   );
--- consultn site/frontend/src/Home.js
+++ consultn site/frontend/src/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import banner from './banner.png';
+import Navbar from './navbar';
+import MapPage from 'map.js';
 
 const Home = () => {
+  const navigateToMap = () => {
+    // Assuming you have a routing library like React Router set up:
+    window.location.href = "/map"; // Navigate to the map page using URL
+  };
+
   return (
     <div className="home">
-      <h1>Welcome to My Home Page</h1>
-      <p>This is the home page content.</p>
+      <img src={banner} alt="Home Page Banner" className="banner" />
+      <Navbar />
+      <button className="map-button" onClick={MapPage}>
+        Map
+      </button>
     </div>
   );
 };
