204527cc52ea654502a918a765c5ca84d553d878
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-06 12:27:23 +0530

    map code

---
+++ Map/frontend/src/mappage.js
@@ -0,0 +1,23 @@
+"use client";
+
+import { useState } from "react";
+import{
+    APIProvider,
+    Map,
+    AdvancedMarker,
+    Pin,
+    InfoWindow,
+} from "@vis.gl/react-google-maps";
+
+export default function Intro() {
+    const position = { lat: 5354, lng: 10 };
+
+    return (
+        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+            <div>
+                <Map zoom={9} center></Map>
+            </div>
+            </APIProvider>
+
+    );
+}
