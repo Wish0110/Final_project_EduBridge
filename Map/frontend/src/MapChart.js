de4da44624625794b7c4ac854b7ca1928638fab1
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-06 14:35:17 +0530

    chngs

--- Map/frontend/src/MapChart.js
+++ Map/frontend/src/MapChart.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Geographies, Geography } from 'react-simple-maps';
-
-const geoUrl =
-"https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css'; // Include CSS for styling
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
 
 const MapChart = () => {
+  // Initial map center and zoom level
+  const mapCenter = [52.3555, -1.1572]; // Center of UK
+  const zoomLevel = 6;
+
   return (
-    <Geographies geographyUrl={geoUrl}>
-      {({ geographies }) =>
-        geographies.map(geo => (
-          <Geography
-            key={geo.rsmKey}
-            geography={geo}
-            fill="#EAEAE7"
-            stroke="#D6D6DA"
+      <MapContainer center={mapCenter} zoom={zoomLevel} style={{ height: '400px' }}>
+          <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-        ))
-      }
-    </Geographies>
+          <ZoomControl position="bottomright" />
+      </MapContainer>
   );
 };
 
