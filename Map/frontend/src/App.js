83f6504bdd3f6cf51738da822ea2d4754b64eb82
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-06 13:12:52 +0530

    chngs

--- Map/frontend/src/App.js
+++ Map/frontend/src/App.js
@@ -1,9 +1,15 @@
-import Intro from './mappage'; 
+import React from 'react';
+import { APIProvider } from '@vis.gl/react-google-maps';
+import Intro from './mappage'; // Adjust the path if necessary
+
+const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 function App() {
   return (
     <div>
-    <Intro />
+      <APIProvider apiKey={googleMapsApiKey}>
+        <Intro />
+      </APIProvider>
     </div>
   );
 }
--- Map/frontend/src/mappage.js
+++ Map/frontend/src/mappage.js
@@ -1,23 +1,12 @@
-"use client";
-
-import { useState } from "react";
-import{
-    APIProvider,
-    Map,
-    AdvancedMarker,
-    Pin,
-    InfoWindow,
-} from "@vis.gl/react-google-maps";
+import React from 'react';
+import { Map } from '@vis.gl/react-google-maps';
 
 export default function Intro() {
-    const position = { lat: 5354, lng: 10 };
+  const position = { lat: 5354, lng: 10 };
 
-    return (
-        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-            <div style={{height: "100vh"}}>
-                <Map zoom={9} center={position}></Map>
-            </div>
-        </APIProvider>
-
-    );
+  return (
+    <div style={{ height: '100vh' }}>
+      <Map zoom={9} center={position}></Map>
+    </div>
+  );
 }
