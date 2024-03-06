7d1fd2522d1461697a7f5f17b5ca5b8763545a5e
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-06 14:56:37 +0530

    point plymouth university

--- Map/frontend/src/MapChart.js
+++ Map/frontend/src/MapChart.js
@@ -1,21 +1,30 @@
 import React from 'react';
 //import L from 'leaflet';
 import 'leaflet/dist/leaflet.css'; // Include CSS for styling
-import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, ZoomControl } from 'react-leaflet';
+import { Popup } from 'react-leaflet';
 
 const MapChart = () => {
   // Initial map center and zoom level
   const mapCenter = [52.3555, -1.1572]; // Center of UK
   const zoomLevel = 6;
 
+  // Coordinates of Plymouth University
+  const plymouthUniversityCoordinates = [50.4122, -4.0882];
+
   return (
-      <MapContainer center={mapCenter} zoom={zoomLevel} style={{ height: '400px' }}>
-          <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <ZoomControl position="bottomright" />
-      </MapContainer>
+    <MapContainer center={mapCenter} zoom={zoomLevel} style={{ height: '400px' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={plymouthUniversityCoordinates}>
+        <Popup>
+          Plymouth University
+        </Popup>
+      </Marker>
+      <ZoomControl position="bottomright" />
+    </MapContainer>
   );
 };
 
