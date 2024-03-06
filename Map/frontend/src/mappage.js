d0036b9c6646ca951a8201a7a7b891bcd537bf80
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-06 12:28:46 +0530

    chng

--- Map/frontend/src/mappage.js
+++ Map/frontend/src/mappage.js
@@ -14,8 +14,8 @@ export default function Intro() {
 
     return (
         <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-            <div>
-                <Map zoom={9} center></Map>
+            <div style={{height: "100vh"}}>
+                <Map zoom={9} center={position}></Map>
             </div>
             </APIProvider>
 
