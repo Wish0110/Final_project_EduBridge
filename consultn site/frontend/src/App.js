3f55c144d85b6e58eb714d4e8710b09134521593
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-28 00:21:24 +0530

    1

--- consultn site/frontend/src/App.js
+++ consultn site/frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
-import Map from './map';
+import Map from './MapChart';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/map" element={<Map />} />
+        <Route path="/MapChart" element={<Map />} />
       </Routes>
     </Router>
   );
--- consultn site/frontend/src/Home.js
+++ consultn site/frontend/src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
-import Map from './map';
+import Map from './MapChart';
 import banner from './banner.png';
 import Navbar from './navbar';
 import './App.css';
@@ -12,10 +12,10 @@ const Home = () => {
       <img src={banner} alt="Home Page Banner" className="banner" />
       <Navbar />
       <Routes>
-        <Route path="/map" element={<Map />} />
+        <Route path="/MapChart" element={<Map />} />
         <Route exact path="/" element={<div>
 
-          <button className="map-button" onClick={() => navigate('/map')}>
+          <button className="map-button" onClick={() => navigate('/MapChart')}>
             Map
           </button>
         </div>} />
