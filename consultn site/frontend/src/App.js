5639e93e981d8cf428baec4e599739d0449e74fc
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-27 19:50:55 +0530

    xhngs

--- consultn site/frontend/src/App.js
+++ consultn site/frontend/src/App.js
@@ -2,14 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
-import Map from './map';
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/map" element={<Map />} />
+        
       </Routes>
     </Router>
   );
