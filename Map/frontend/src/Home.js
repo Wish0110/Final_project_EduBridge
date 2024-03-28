cfec06cf8d79bf9fb041d663a708ddbe1c4d9593
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-28 13:58:10 +0530

    map add

--- Map/frontend/src/Home.js
+++ Map/frontend/src/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import MapChart from './MapChart';
+import banner from './banner.png';
+import Navbar from './navbar';
+import './App.css';
 
 const App = () => {
   return (
     <div>
-      <h1>Universities in UK</h1>
+      <Navbar />
+      <header style={{ textAlign: 'center', marginBottom: '1rem' }}>
+      <img src={banner} alt="Home Page Banner" className="banner" />
+      </header>
+      <h2>UK University Map</h2>
       <MapChart />
     </div>
   );
