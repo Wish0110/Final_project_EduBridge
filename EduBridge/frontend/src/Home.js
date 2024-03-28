4809f3a525fd5238ffbd2afcf34f03e245364e45
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-28 14:14:12 +0530

    main webapp create

---
+++ EduBridge/frontend/src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import MapChart from './MapChart';
+import banner from './banner.png';
+import Navbar from './navbar';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <header style={{ textAlign: 'center', marginBottom: '1rem' }}>
+      <img src={banner} alt="Home Page Banner" className="banner" />
+      </header>
+      <h2>UK University Map</h2>
+      <MapChart />
+    </div>
+  );
+};
+
+export default App;
