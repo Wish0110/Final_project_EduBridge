5f77ec16321eca7bacbf6675d642703b25cc957b
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-28 10:06:19 +0530

    rmv

--- Map/frontend/src/App.js
+++ Map/frontend/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
 import MapChart from './MapChart';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/map" element={<MapChart />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Universities in UK</h1>
+      <MapChart />
+    </div>
   );
 };
 
