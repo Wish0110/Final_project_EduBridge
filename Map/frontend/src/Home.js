a957e185ae4f7771825fc612302a15b0ad142628
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-28 12:27:12 +0530

    chngs

---
+++ Map/frontend/src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapChart from './MapChart';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Universities in UK</h1>
+      <MapChart />
+    </div>
+  );
+};
+
+export default App;
