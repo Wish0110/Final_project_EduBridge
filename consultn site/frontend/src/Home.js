8f4fbd9664698e352ce02615519058da22192e0b
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-28 11:32:46 +0530

    rmv

--- consultn site/frontend/src/Home.js
+++ consultn site/frontend/src/Home.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Map from './map';
 import banner from './banner.png';
 import Navbar from './navbar';
-import MapButton from './MapButton';
 import './App.css';
 
 const Home = () => {
@@ -12,9 +10,7 @@ const Home = () => {
       <img src={banner} alt="Home Page Banner" className="banner" />
       <Navbar />
       <Routes>
-        <Route path="/map" element={<Map />} />
         <Route exact path="/" element={<div>
-          <MapButton />
         </div>} />
       </Routes>
     </div>
