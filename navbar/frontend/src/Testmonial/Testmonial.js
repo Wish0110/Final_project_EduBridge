8ae7b79dbdde2e1736a8489fda96a8e4ec06c621
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-29 09:32:41 +0530

    testmonial code

---
+++ navbar/frontend/src/Testmonial/Testmonial.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Testmonial = () => {
+    return (
+        <div className="container">
+
+            <h1 className="text-center" style={{paddingTop: "30%"}}>
+                Testmonial
+            </h1>
+
+        </div>
+    )
+}
+export default Testmonial;
