4427d6bf819fc62bd8966305416b623667797312
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-29 10:06:04 +0530

    nav bar js with routes

--- navbar/frontend/src/App.js
+++ navbar/frontend/src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import Service from './pages/Service/Service';
+import Testmonial from './pages/Testmonial/Testmonial'; 
+import Navbar from './Componenets/Navbar/Navbar';
 
 const App = () => {
   return (
-    <h1>Hello world</h1>
+    <Router>
+      <Navbar/>
+      <main>
+      <Routes>
+        <Route path="/" exact 
+          element={<Home />} />
+        <Route path="/About" exact 
+          element={<About />} />
+        <Route path="/Contact" exact 
+          element={<Contact />} />
+        <Route path="/Service" exact 
+          element={<Service />} />
+        <Route path="/Tesmonial" exact 
+          element={<Testmonial />} />
+    </Routes>
+          
+      </main>
+    </Router>
   );
 };
 
---
+++ navbar/frontend/src/Componenets/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="container">
+
+            <h1 className="text-center" style={{paddingTop: "30%"}}>
+                 Navbar
+            </h1>
+
+        </div>
+    )
+}
+export default Navbar;
