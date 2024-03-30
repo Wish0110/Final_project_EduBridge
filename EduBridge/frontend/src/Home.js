815b417cee54193068c780138e3cac53987359fd
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-30 13:31:30 +0530

    chngs

--- EduBridge/frontend/src/Home.js
+++ EduBridge/frontend/src/Home.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import MapChart from './MapChart';
 import banner from './banner.png';
-import Navbar from './Componenets/Navbar/Navbar';
 import './App.css';
 
-const App = () => {
+const Home = () => {
   return (
-    <div>
-      <Navbar />
-      <header style={{ textAlign: 'center', marginBottom: '1rem' }}>
+    <div className="container">
+
       <img src={banner} alt="Home Page Banner" className="banner" />
-      </header>
+
       <h2>UK University Map</h2>
       <MapChart />
     </div>
   );
 };
 
-export default App;
+export default Home;
