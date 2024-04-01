d7417abf3451b2cb4cf3d4da582032af0013202c
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-01 10:10:11 +0530

    Home css apply

--- EduBridge/frontend/src/App.js
+++ EduBridge/frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
-import Home from './Home';
+import Home from './pages/Home/Home';
 import Service from './pages/Service/Service';
 import Testmonial from './pages/Testmonial/Testmonial'; 
 import Navbar from './Componenets/Navbar/Navbar';
--- EduBridge/frontend/src/pages/Home/Home.js
+++ EduBridge/frontend/src/pages/Home/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import MapChart from './MapChart';
 import banner from './banner.png';
+import './Home.css';
 
 const Home = () => {
   return (
     <div className="container">
-
       <img src={banner} alt="Home Page Banner" className="banner" />
-
-      <h2>UK University Map</h2>
-      <MapChart />
+      <div className="map-chart-container">
+        <h2>UK University Map</h2>
+        <MapChart />
+      </div>
     </div>
   );
 };
