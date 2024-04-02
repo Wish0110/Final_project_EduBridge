8c5c528f4cd770a65b9db481cfaa1fff576bbde8
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-02 20:29:16 +0530

    css in sidebar

--- form/frontend/src/components/Sidebar.js
+++ form/frontend/src/components/Sidebar.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import {
+    FaBars,
+    FaCommentAlt,
+    FaRegChartBar,
+    FaTh, FaUserAlt
+}from 'react-icons/fa';
 
 const Sidebar = () => {
+    const menuItem=[
+        {
+            path:'/',
+            name:'Dashboard',
+            icon:<FaTh/>
+        },
+        {
+            path:'/about',
+            name:'About',
+            icon:<FaUserAlt/>
+        },
+        {
+            path:'/comment',
+            name:'Comment',
+            icon:<FaCommentAlt/>
+        },
+        {
+            path:'/analytics',
+            name:'Analytics',
+            icon:<FaRegChartBar/>
+        }
+
+]
   return (
-    <div className="sidebar">
-      <h2>Sidebar</h2>
+    <div className="container">
+      <div className="sidebar">
+        <div className='top_section'>
+            <h1 className='logo'>Logo</h1>
+            <div className='bars'>
+                <FaBars/>
+            </div>
+        </div>
+        {
+            
+        }
+      </div>  
     </div>
   );
 }
