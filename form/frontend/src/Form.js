eb35d409599501a85d2322fb7cf9b9d4e4a86ca4
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-02 16:00:07 +0530

    sidebar import

--- form/frontend/src/Form.js
+++ form/frontend/src/Form.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Sidebar from './Sidebar';
 
 function App () {
 
   return (
     <div className='d-flex'>
       <div className='col-auto'>
-
+        <Sidebar />
     </div>     
     </div>
   );
