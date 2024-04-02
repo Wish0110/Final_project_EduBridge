b0703ceb9b3cd40bd7d75048871bfe873c7ae7f3
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-02 20:37:17 +0530

    display

--- form/frontend/src/components/Sidebar.js
+++ form/frontend/src/components/Sidebar.js
@@ -5,8 +5,9 @@ import {
     FaRegChartBar,
     FaTh, FaUserAlt
 }from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 
-const Sidebar = () => {
+const Sidebar = ({Children}) => {
     const menuItem=[
         {
             path:'/',
@@ -40,9 +41,15 @@ const Sidebar = () => {
             </div>
         </div>
         {
-            
+            menuItem.map((item,index)=>(
+                <NavLink to={(item.path)} key={index} className='link' activeclassName="active">
+                            <div className='icon'>{item.icon}</div>
+                            <div className='link_text'>{item.name}</div>
+                </NavLink>
+            ))
         }
       </div>  
+      <main>{Children}</main>
     </div>
   );
 }
