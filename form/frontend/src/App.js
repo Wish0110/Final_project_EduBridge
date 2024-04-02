ecedf6851ece646622631318a86fa8c180d38ab3
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-02 21:56:43 +0530

    styles done

--- form/frontend/src/App.js
+++ form/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
