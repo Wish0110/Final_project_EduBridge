d5c50d08dbcdf5c77e223fde0fe2d0ab5922cdbf
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-02 16:09:34 +0530

    install

--- form/frontend/src/App.js
+++ form/frontend/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+//import { ReactDOM } from "react-dom/client";
 import Form from "./Form"; 
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Form />
   </React.StrictMode>
   
-)
--- form/frontend/src/Sidebar.js
+++ form/frontend/src/Sidebar.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 function Sidebar() {
   return (
-    <div>
-      
+    <div className='d-flex flex-column'>
+        <a href="d-flex align-items-center">
+            <i className='bi bi-bootstrap'></i>
+            <span>Sidebar</span>
+        </a>
     </div>
   )
 }
