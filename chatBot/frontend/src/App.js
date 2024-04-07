1c36a3542926c50528c3fe73ff9e5b7f7624fbb3
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-07 13:04:12 +0530

    chngs

--- chatBot/frontend/src/App.js
+++ chatBot/frontend/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import '@chatscope/chat-ui-kit-react/dist/default/styles.min.css';
-import { MainContsiner, ChatContainer, MessageList, MessageGroup, MessageInput, TypingIndicator} from '@chatscope/chat-ui-kit-react';
+import { MainContainer, ChatContainer, MessageList, MessageGroup, MessageInput, TypingIndicator} from '@chatscope/chat-ui-kit-react';
 
 function App() {
+  // eslint-disable-next-line no-undef
+  const [messages, setMessages] = useState([
+  {
+    message: "Hello, how can I help you?",
+    sender: "chatbot"
+  }
+ ])
+
   return (
     <div className="App">
       <div style={{ position: "relative", height: '800vh', width:"700px"}}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+
+            </MessageList>
+          </ChatContainer>
+        </MainContainer>
       </div>
     </div>
   );
