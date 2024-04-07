01abffb75edeff736cc44968866475ec3d8c13b5
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-07 14:36:43 +0530

    chngs

--- chatBot/frontend/src/App.js
+++ chatBot/frontend/src/App.js
@@ -1,42 +1,49 @@
 import React, { useState } from 'react';
 import './App.css';
-import '@chatscope/chat-ui-kit-styles/dist/default-styles.min.css';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import {
   MainContainer,
   ChatContainer,
   MessageList,
   Message,
   MessageInput,
+  TypingIndicator
 } from '@chatscope/chat-ui-kit-react';
 
 function App() {
+  const [typing, setTyping] = useState(false);
   const [messages, setMessages] = useState([
     {
-      message: "Hello, how can I help you?",
-      sender: "chatbot"
+      message: "Hello, I am ChatGPT!",
+      sender: "ChatGpt",
     }
-  ])
+  ]) //[]
 
   const handleSend = async (message) => {
     const newMessage = { 
       message: message,
       sender: "user",
-      direction: "outgoing" 
+      direction: "outgoing" ,
     }
 
-    const newMessages = [...messages, newMessage] // old and new messages
+    const newMessages = [...messages, newMessage]; // old and new messages
 
     // update messages state
     setMessages(newMessages);
+
+    //typing indicatore
+    setTyping(true);
     //process user message
   }
 
   return (
     <div className="App">
-      <div style={{ position: "relative", height: '800vh', width:"700px"}}>
+      <div style={{ position: "relative", height: '700px', width:"700px"}}>
         <MainContainer>
           <ChatContainer>
-            <MessageList>
+            <MessageList
+            typingIndicator={typing ? <TypingIndicator content="Mocha is typing" /> :null}
+            >
               {messages.map((message, i) => {
                 return <Message key={i} model={message} />
               })}
