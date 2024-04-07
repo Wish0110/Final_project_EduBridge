bca521db25d5c91bf743200cfe661b1f4e34ac86
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-07 12:57:49 +0530

    styles

--- chatBot/frontend/src/App.js
+++ chatBot/frontend/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import '@chatscope/chat-ui-kit-react/dist/default/styles.min.css';
+import { MainContsiner, ChatContainer, MessageList, MessageGroup, MessageInput, TypingIndicator} from '@chatscope/chat-ui-kit-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ position: "relative", height: '800vh', width:"700px"}}>
+      </div>
     </div>
   );
 }
