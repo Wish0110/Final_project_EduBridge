493400fbdbb6dd48fa1dab137181abafd8b92cbc
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-09 10:35:30 +0530

    install pchg

--- Letter_Generetation/frontend/src/App.js
+++ Letter_Generetation/frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from 'react';
 import axios from 'axios';
 import html2pdf from 'html2pdf.js';
-import { Document, Page } from 'react-pdf';
 import './App.css';
 
 function App() {
