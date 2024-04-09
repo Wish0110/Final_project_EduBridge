2e52d16a1da50985d7b184f93a95ad93732e2c20
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-09 11:36:59 +0530

    add side bar

--- EduBridge/frontend/src/pages/form/pages/Education.js
+++ EduBridge/frontend/src/pages/form/pages/Education.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Sidebar from '../components/Sidebar';
 
 const Education = () => {
   return (
+    
     <div>
+      <Sidebar />
       <h2>Education</h2>
     </div>
   );
