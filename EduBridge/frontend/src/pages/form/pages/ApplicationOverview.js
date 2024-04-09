3425372f1d6bd94178b7a57b25a876fa454c6424
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-09 22:31:29 +0530

    buttons add

--- EduBridge/frontend/src/pages/form/pages/ApplicationOverview.js
+++ EduBridge/frontend/src/pages/form/pages/ApplicationOverview.js
@@ -36,6 +36,7 @@ const ApplicationOverview = () => {
             <div className="card-content">
               <h3>Supporting Information</h3>
               <p>So that providers know how to support you during your studies.</p>
+              <button className="start-button">Start this section</button>
             </div>
           </div>
           <div className="card">
@@ -44,12 +45,14 @@ const ApplicationOverview = () => {
               <h3>English Language Skills</h3>
               </div>
               <p>Is English your first language?</p>
+              <button className="start-button">Start this section</button>
             </div>
           </div>
           <div className="card">
             <div className="card-content">
               <h3>Finance And Funding</h3>
               <p>Tell us how you’ll fund your study.</p>
+              <button className="start-button">Start this section</button>
             </div>
           </div>
         </div>
@@ -58,18 +61,21 @@ const ApplicationOverview = () => {
             <div className="card-content">
               <h3>Education</h3>
               <p>Qualifications and periods of study.</p>
+              <button className="start-button">Start this section</button>
             </div>
           </div>
           <div className="card">
             <div className="card-content">
               <h3>Reccomandation Letter</h3>
               <p>This is card 8.</p>
+              <button className="start-button">Start this section</button>
             </div>
           </div>
           <div className="card">
             <div className="card-content">
               <h3>Employment</h3>
               <p>Paid employment.</p>
+              <button className="start-button">Start this section</button>
             </div>
           </div>
         </div>
@@ -78,6 +84,7 @@ const ApplicationOverview = () => {
             <div className="card-content">
               <h3>Personal statement</h3>
               <p>Why do you want to study this subject?</p>
+              <button className="start-button">Start this section</button>
             </div>
           </div>
         </div>
