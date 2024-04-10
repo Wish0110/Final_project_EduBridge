3396db933fd88b67cdfe9be7954af891279ccfc4
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-10 17:01:59 +0530

    chatbot button

--- EduBridge/frontend/src/pages/Home/Home.js
+++ EduBridge/frontend/src/pages/Home/Home.js
@@ -6,14 +6,23 @@ import Navbar from '../../Componenets/Navbar/Navbar';
 
 const Home = () => {
   return (
+
     <div className='Navbar'>
     <Navbar />
+
     <div className="HomeContainer">
+
       {/*<img src={banner} alt="Home Page Banner" className="banner" />*/}
       <div className="map-chart-container">
         <h2>UK University Map</h2>
         <MapChart />
       </div>
+
+    {/*chatbot*/}
+    <div className="sticky-button">
+         <button>Button</button>
+    </div>
+
     </div>
     </div>
   );
