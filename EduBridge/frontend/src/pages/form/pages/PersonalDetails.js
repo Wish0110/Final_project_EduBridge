7e7219bcb750d8dac4d49a723b408ec2564ce229
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-15 15:11:28 +0530

    import css

--- EduBridge/frontend/src/pages/form/pages/PersonalDetails.js
+++ EduBridge/frontend/src/pages/form/pages/PersonalDetails.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import './form-css/PersonalDetails.css';
 import Sidebar from "../components/Sidebar";
 
 const PersonalDetails = () => {
