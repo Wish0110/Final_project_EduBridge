a73928be0e00c7d28daf2fcc8ac954ee5d57c125
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-15 16:32:20 +0530

    recreate done

--- EduBridge/frontend/src/pages/form/pages/NationalityDetails.js
+++ EduBridge/frontend/src/pages/form/pages/NationalityDetails.js
@@ -7,6 +7,8 @@ const NationalityDetails = () => {
   const [birthCountry, setBirthCountry] = useState('');
   const [arrivalDate, setArrivalDate] = useState('');
   const [nationality, setNationality] = useState('');
+  const [dualcountry, setDualCountry] = useState('');
+  const [showQuestions, setShowQuestions] = useState(false);
 
   const handleBirthCountryChange = (event) => {
     setBirthCountry(event.target.value);
@@ -18,6 +20,11 @@ const NationalityDetails = () => {
 
   const handleNationalityChange = (event) => {
     setNationality(event.target.value);
+  };
+
+  const handleDualNationalityChange = (event) => {
+    setDualCountry(event.target.value);
+    setShowQuestions(event.target.value !== "");
   };
 
   return (
@@ -48,6 +55,55 @@ const NationalityDetails = () => {
         <option value="sri-lankan">Sri Lankan</option>
         {/* Add more nationality options here */}
       </select>
+      <label htmlFor="dualnationality">
+        Dual nationality <br />
+        If you have dual nationality, select your first nationality in the previous field and your second nationality here.
+      </label>
+      <select id="dualcountry" value={dualcountry} onChange={handleDualNationalityChange}>
+        <option value="">Select a country</option>
+        <option value="UK">UK</option>
+        {/* Add more country options here */}
+      </select>
+      {showQuestions && (
+          <>
+            <h3>Additional questions</h3>
+            <p>Do you need a student visa to study in the UK?</p>
+            <label>
+              <input type="radio" name="visa" value="yes" />
+              Yes
+            </label>
+            <label>
+              <input type="radio" name="visa" value="no" />
+              No
+            </label>
+
+            <p>Have you previously studied on a student or tier 4 visa?</p>
+            <label>
+              <input type="radio" name="prevStudied" value="yes" />
+              Yes
+            </label>
+            <label>
+              <input type="radio" name="prevStudied" value="no" />
+              No
+            </label>
+
+            <p>Do you have settled or pre-settled status in the UK?</p>
+            <label>
+              <input type="radio" name="settledStatus" value="yes" />
+              Yes
+            </label>
+            <label>
+              <input type="radio" name="settledStatus" value="no" />
+              No
+            </label>
+          </>
+        )}
+        <label>
+            <input type="checkbox" />
+            Mark this section as complete
+          </label>
+          <br />
+          <button type="button">Save this section</button>
     </form>
     </Sidebar>
   );
