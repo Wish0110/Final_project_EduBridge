5d4293c5f71b3ec9de2504ff9ac467b234b504de
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-17 16:28:06 +0530

    remove navbar

--- EduBridge/frontend/src/pages/Contact/Contact.js
+++ EduBridge/frontend/src/pages/Contact/Contact.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "../../Componenets/Navbar/Navbar";
 import "./Contact.css"
 
 const Contact = () => {
     return (
-        <div className='Navbar'>
-            <Navbar />
+
         <div className="container">
 
             <h1 className="text-center" style={{paddingTop: "30%"}}>
@@ -13,7 +11,7 @@ const Contact = () => {
             </h1>
 
         </div>
-        </div>
+
     )
 }
 export default Contact;
