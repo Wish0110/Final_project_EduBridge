6ee5e89d5fbd435c230424334cb2d8b2946cb97d
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-19 14:11:23 +0530

    styles for plymouth uni

--- EduBridge/frontend/src/pages/Universities/PlymouthUnivesity.js
+++ EduBridge/frontend/src/pages/Universities/PlymouthUnivesity.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import './PlymouthUnivesity.css';
 //import CourseLink from './CourseLink';
 
 const PlymouthUnivesity = () => {
