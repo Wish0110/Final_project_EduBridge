ec070b011af8b8c7ba4462497b832f49ca13b8cd
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-19 14:14:11 +0530

    wrap

--- EduBridge/frontend/src/pages/Universities/PlymouthUnivesity.js
+++ EduBridge/frontend/src/pages/Universities/PlymouthUnivesity.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './PlymouthUnivesity.css';
+import './PlymouthUniversity.css';
 //import CourseLink from './CourseLink';
 
 const PlymouthUnivesity = () => {
