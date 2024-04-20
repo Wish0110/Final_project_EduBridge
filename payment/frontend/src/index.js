084899437f65da31c88bc58b635b68b0277d9c21
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-20 13:39:27 +0530

    chngs

--- payment/frontend/src/index.js
+++ payment/frontend/src/index.js
@@ -1,24 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
-import 'babel-polyfill'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { LoadingIndicator } from './components/LoadingIndicator';
+import { CheckoutForm } from './components/CheckoutForm';
 
-const products = [
-  {
-    id: 1,
-    name: 'Product 1',
-    price: 1000
-  },
-  {
-    id: 2,
-    name: 'Product 2',
-    price: 2000
-  },
-  {
-    id: 3,
-    name: 'Product 3',
-    price: 3000
-  }
-]
-
-ReactDOM.hydrate(<App products={products} />, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <LoadingIndicator />
+    <CheckoutForm />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
