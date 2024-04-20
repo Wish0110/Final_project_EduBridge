7422020daaccade5b6d8843e12d7a3b3ebf8f9b6
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-20 23:06:51 +0530

    rmv

--- EduBridge/frontend/src/pages/About/About.js
+++ EduBridge/frontend/src/pages/About/About.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./About.css";
-import Card from "./Card";
+import ReactCardFlip from "react-card-flip";
 
-const About = () => {
-  return (
-    <div className="how-to-use">
-      <div className="row">
-      <Card />
+function About() {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    function flipCard() {
+        setIsFlipped(!isFlipped);
+    }
+
+    return (
+        <div>
+            <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped}>
+                <div className="card" onClick={flipCard}>
+                    <h1>Front</h1>
+                </div>
+                <div className="card card-back" onClick={flipCard}>
+                    <h1>Back</h1>
+                </div>
+            </ReactCardFlip>
+        </div>
         
-      </div>
-    </div>
-  );
+    );
 };
-
 export default About;
