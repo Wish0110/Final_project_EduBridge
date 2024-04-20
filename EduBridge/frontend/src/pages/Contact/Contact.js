d5f1b8a1943ca3da479b101610b31b162bf6d165
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-20 19:49:01 +0530

    chngs

--- EduBridge/frontend/src/pages/Contact/Contact.js
+++ EduBridge/frontend/src/pages/Contact/Contact.js
@@ -2,16 +2,17 @@ import React from "react";
 import "./Contact.css"
 
 const Contact = () => {
-    return (
 
-        <div className="container">
+        return (
+            <footer className="footer">
+              <p>&copy; 2023 EduBridge - Bridge to a successful university application in the UK.</p>
+              <ul>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </footer>
+          );
+        };
 
-            <h1 className="text-center" style={{paddingTop: "30%"}}>
-                Contact
-            </h1>
-
-        </div>
-
-    )
-}
 export default Contact;
