cfb9c964509174a683696e18a9621f5c141ac0af
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-20 22:51:08 +0530

    rmv

--- EduBridge/frontend/src/pages/About/Card.js
+++ EduBridge/frontend/src/pages/About/Card.js
@@ -1,52 +1,20 @@
 import React, { useState } from "react";
-import "./Card.css";
+import ReactCardFlip from "react-card-flip";
 
 const Card = () => {
-    const cardFront = "Welcome to GFG.";
-    const cardBack = "Computer Science Portal.";
-    const [isFlipped, setFlipped] = useState(false);
- 
-    const handleFlip = () => {
-        setFlipped(!isFlipped);
-    };
- 
+    
     return (
-        <div className="App">
-            <h1 className="geeks">GeeksforGeeks</h1>
-            <h3>React Example for Flip Card Effect</h3>
-            <div className="container">
-                <div
-                    className={`flip-card ${
-                        isFlipped ? "flipped" : ""
-                    }`}
-                >
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                            <div className="card-content">
-                                {cardFront}
-                            </div>
-                            <button
-                                className="flip-button"
-                                onClick={handleFlip}
-                            >
-                                Flip
-                            </button>
-                        </div>
-                        <div className="flip-card-back">
-                            <div className="card-content">
-                                {cardBack}
-                            </div>
-                            <button
-                                className="flip-button"
-                                onClick={handleFlip}
-                            >
-                                Flip
-                            </button>
-                        </div>
-                    </div>
+        <div>
+            <ReactCardFlip>
+                <div className="card">
+                    <h1>Front</h1>
                 </div>
-            </div>
+                <div className="card card-back">
+                    <h1>Back</h1>
+                </div>
+            </ReactCardFlip>
         </div>
+        
     );
 };
 
