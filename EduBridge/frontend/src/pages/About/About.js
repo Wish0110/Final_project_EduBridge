3087911f7fa3bfadfafaa2b5a15fd51e0e6c66d4
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-20 22:57:36 +0530

    chngs

--- EduBridge/frontend/src/pages/About/About.js
+++ EduBridge/frontend/src/pages/About/About.js
@@ -6,22 +6,8 @@ const About = () => {
   return (
     <div className="how-to-use">
       <div className="row">
-        <Card
-          front="Step 1"
-          back="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."
-        />
-        <Card
-          front="Step 2"
-          back="Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam."
-        />
-        <Card
-          front="Step 3"
-          back="Nam at tortor in tellus interdum sagittis. Donec posuere metus vitae ipsum. Integer a nibh."
-        />
-        <Card
-          front="Step 4"
-          back="Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna."
-        />
+      <Card />
+        
       </div>
     </div>
   );
--- EduBridge/frontend/src/pages/About/Card.js
+++ EduBridge/frontend/src/pages/About/Card.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import ReactCardFlip from "react-card-flip";
 
 const Card = () => {
-    
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    function flipCard() {
+        setIsFlipped(!isFlipped);
+    }
+
     return (
         <div>
-            <ReactCardFlip>
+            <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped}>
                 <div className="card">
                     <h1>Front</h1>
                 </div>
