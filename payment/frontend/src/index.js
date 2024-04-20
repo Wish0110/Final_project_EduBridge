20867968087eb0a3233b91513f8b0325dad35771
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-20 13:37:34 +0530

    chngs

--- payment/frontend/src/index.js
+++ payment/frontend/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import 'babel-polyfill'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 1000
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 2000
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 3000
+  }
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.hydrate(<App products={products} />, document.getElementById('root'))
