a887aff8a19f6d32d287a8861d9b2ffb3717eec8
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-22 00:22:15 +0530

    chngs

--- EduBridge/frontend/src/pages/integratedUni/IsIntegrated.js
+++ EduBridge/frontend/src/pages/integratedUni/IsIntegrated.js
@@ -2,25 +2,22 @@ import React from 'react';
 import './IsIntegrated.css';
 
 const universityOptions = [
-  { name: 'NSBM Green University', isCorrect: false },
-  { name: 'Integrated University 1', isCorrect: false },
-  { name: 'Integrated University 2', isCorrect: true },
-  { name: 'Integrated University 3', isCorrect: false },
-  { name: 'Integrated University 4', isCorrect: false },
+    { name: 'NSBM Green University', isCorrect: false },
+    { name: 'Integrated University 1', isCorrect: false },
+    { name: 'Integrated University 2', isCorrect: false },
+    { name: 'Integrated University 3', isCorrect: false },
+    { name: 'Integrated University 4', isCorrect: false },
 ];
 
 const UniversityChoice = () => {
   const [selectedUniversity, setSelectedUniversity] = React.useState(null);
-  const [showNoButton, setShowNoButton] = React.useState(false);
 
   const handleUniversityClick = (university) => {
     setSelectedUniversity(university);
-    setShowNoButton(true);
   };
 
   const handleNoClick = () => {
     setSelectedUniversity(null);
-    setShowNoButton(false);
   };
 
   return (
@@ -31,18 +28,15 @@ const UniversityChoice = () => {
           <button
             key={index}
             className={`university-button ${selectedUniversity === university ? 'selected' : ''}`}
-            disabled={showNoButton}
             onClick={() => handleUniversityClick(university)}
           >
             {university.name}
           </button>
         ))}
       </div>
-      {showNoButton && (
-        <button className="no-button" onClick={handleNoClick}>
-          No
-        </button>
-      )}
+      <button className="no-button" onClick={handleNoClick}>
+        No
+      </button>
     </div>
   );
 };
