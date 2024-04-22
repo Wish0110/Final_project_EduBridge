0b5e13c81c5926cad83931347c7a20d5ed226fa9
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-22 12:57:17 +0530

    add 4 cads

--- EduBridge/frontend/src/pages/About/About.js
+++ EduBridge/frontend/src/pages/About/About.js
@@ -1,33 +1,24 @@
-import React, {useState} from 'react';
-import { Card } from 'react-bootstrap';
-import './About.css';
+import React from 'react';
+import Card from './card';
 
-const StepCard = ({ title, children }) => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  const handleClick = () => {
-    setIsFlipped(!isFlipped);
-  };
+const About = () => {
 
   return (
-    <div className="Home-Cards">
-      <div className={`flip-card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
-        <div className="flip-card-inner">
-          <Card className="flip-card-front" style={{ width: '18rem' }}>
-            <Card.Header className="text-center">{title}</Card.Header>
-            <Card.Body style={{ border: '1px solid black', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
-              {children}
-            </Card.Body>
-          </Card>
-          <Card className="flip-card-back" style={{ width: '18rem' }}>
-            <Card.Body style={{ border: '1px solid black', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
-              <Card.Text>Back side of the card for {title}</Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-      </div>
+    <>
+    <div className="card1">
+      <Card />
     </div>
+    <div className="card2">
+        <Card />
+    </div>
+    <div className="card3">
+        <Card />
+    </div>
+    <div className="card4">
+        <Card />
+    </div>
+    </>
   );
 };
 
-export default StepCard;
+export default About;
