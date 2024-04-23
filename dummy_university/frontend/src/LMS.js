c2e9dd607fc3dd3c039372972032e5a1b02df3c5
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-23 12:57:44 +0530

    lms page create

---
+++ dummy_university/frontend/src/LMS.js
@@ -0,0 +1,13 @@
+// LMS.js
+import React from 'react';
+
+function LMS() {
+  return (
+    <div>
+      <h3>Learning Management Systems (LMS) & Examinations</h3>
+      {/* Display your database details here */}
+    </div>
+  );
+}
+
+export default LMS;
