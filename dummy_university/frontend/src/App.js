ed2e8c7bdd00d3cb937c00bd9350c889dbb8605a
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-23 15:36:32 +0530

    new folder

--- dummy_university/frontend/src/App.js
+++ dummy_university/frontend/src/App.js
@@ -1,62 +1,12 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button } from 'react-bootstrap';
-import './App.css'
+import './App.css';
+import UniversityChoice from './UniversityChoice';
 
-const App  = ({ link }) => {
+function App() {
   return (
-    <Container>
-      <Row className="g-4">
-        <Col>
-          <Card className="h-100" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card 1</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card className="h-100" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card 2</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card className="h-100" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card 3</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card className="h-100" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card 4</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </Card.Text>
-              <Button className="btn" variant="primary" onClick={() => { window.location.href = link; }}>
-                Go to LMS
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <div className="App">
+      <UniversityChoice />
+    </div>
   );
 }
 
