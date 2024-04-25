75e034e339b70bbdb59e651fcf707991956082b1
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-04-25 15:41:28 +0530

    chngs

--- Paypal/index.js
+++ Paypal/index.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 const fetch =  require("node-fetch") ;
-const dotenv = require('dotenv').config();
-const app = express();
+const dotenv = require('dotenv');
+dotenv.config();const app = express();
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
