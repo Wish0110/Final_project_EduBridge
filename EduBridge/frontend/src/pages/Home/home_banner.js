459a62809574f6d318f5352adc0edf8fc46724fa
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-05-07 12:14:54 +0530

    banner change

--- EduBridge/frontend/src/pages/Home/home_banner.js
+++ EduBridge/frontend/src/pages/Home/home_banner.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './banner.css';
-import img from './Home-Banner.svg';
+import img from './Home-Banner.png';
 
 const HomeBanner = () => {
   return (
     <div className="banner">
-      <div className="banner-text">
-        <h1>Welcome to EduBridge</h1>
-        <p>Your bridge to a successful university application in the UK.</p>
-      </div>
+     
       <div className="banner-image">
         <img src={img} alt="Poster" />
       </div>
-    </div>
+
+      <div className='banner-footer'>
+
+      </div>
+      
+      </div>
   );
 };
 
