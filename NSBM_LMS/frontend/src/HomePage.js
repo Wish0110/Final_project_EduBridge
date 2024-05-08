9907183457faa7c3886b2a0af1b29f422af0f8f5
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-05-08 11:03:24 +0530

    chngs

--- NSBM_LMS/frontend/src/HomePage.js
+++ NSBM_LMS/frontend/src/HomePage.js
@@ -5,12 +5,26 @@ import './home.css';
 const HomePage = () => {
   return (
     <div className="home-page">
+
+      <div className='slot1'>
       <button className="btn btn-primary">Button 1</button>
+      </div>
+
+      <div className='slot2'>
       <button className="btn btn-secondary">Button 2</button>
+      </div>
+
+      <div className='slot3'>
       <button className="btn btn-success">Button 3</button>
+      </div>
+
+      <div className='slot4'>
       <Link to="/students">
         <button className="btn btn-danger">Button 4</button>
-      </Link>    </div>
+      </Link>    
+      </div>
+      
+      </div>
   );
 };
 
