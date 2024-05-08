43f41814d9e6a6be5073a0af7f2686e4d6b02527
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-05-08 11:54:15 +0530

    chngs

--- NSBM_LMS/frontend/src/HomePage.js
+++ NSBM_LMS/frontend/src/HomePage.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Nspot from './top.jpg';
 import './home.css';
 
 const HomePage = () => {
   return (
     <div className="home-page">
 
-      <div className='slot1'>
-      <button className="btn btn-primary">Button 1</button>
+      <div className="slots-container">
+      <img src={Nspot} alt="NSBM Logo" className="nspot"/>
+      <div className='slot slot1'>
+      <button className="btn btn-primary">NLEARN - NSBM LMS</button>
       </div>
 
-      <div className='slot2'>
-      <button className="btn btn-secondary">Button 2</button>
+      <div className='slot slot2'>
+      <button className="btn btn-secondary">PLYMOUTH UNIVERSITY</button>
       </div>
 
-      <div className='slot3'>
-      <button className="btn btn-success">Button 3</button>
+      <div className='slot slot3'>
+      <button className="btn btn-success">VICTORIA UNIVERSITY</button>
       </div>
 
-      <div className='slot4'>
+      <div className='slot slot4'>
       <Link to="/students">
-        <button className="btn btn-danger">Button 4</button>
+        <button className="btn btn-danger">STUDENT PROFILE</button>
       </Link>    
       </div>
-      
+      </div>
       </div>
   );
 };
