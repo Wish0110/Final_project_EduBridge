9da2744b4a524189520e3249d255d07f2ddc4f24
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-05-12 09:17:01 +0530

    chngs

--- EduBridge/frontend/src/pages/form/pages/PersonalStatement.js
+++ EduBridge/frontend/src/pages/form/pages/PersonalStatement.js
@@ -1,43 +1,56 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from '../components/Sidebar';
 
 const PersonalStatement = () => {
-
   const [value, setValue] = useState('');
   const [charCount, setCharCount] = useState(0);
 
   const handleChange = (event) => {
     setValue(event.target.value);
     setCharCount(event.target.value.length);
-  }
+  };
+
   useEffect(() => {
     setCharCount(value.length);
   }, [value]);
 
+  const handleApplicationFeeClick = () => {
+    window.location.href = 'http://localhost:4000';
+  };
+
   return (
     <Sidebar>
-    <div>
-    <label>
-        <b>What will be your main source of funding for your studies?</b><br />
+      <div>
+        <label>
+          <b>What will be your main source of funding for your studies?</b><br />
         </label>
         <label>
-        •Guide to writing your personal statement (opens in a new window) <br /> should help you complete this section.<br />
-        •We strongly recommend you write the statement using a word-processor and <br />paste it in to your application.<br />
-        •You can type your statement directly into the box or edit a statement you have pasted in.<br />
+          •Guide to writing your personal statement (opens in a new window) <br />
+          should help you complete this section.<br />
+          •We strongly recommend you write the statement using a word-processor and <br />
+          paste it in to your application.<br />
+          •You can type your statement directly into the box or edit a statement you have pasted in.<br />
         </label>
         <textarea value={value} onChange={handleChange} /><br />
         <b>You have used {charCount} of 4000 characters</b><br />
 
-        <div><button type="submit">Save</button></div>
-
+        <div>
+          <button type="submit">Save</button>
+        </div>
         <label>
-        •After completing all the application steps, proceed to pay the application fee.<br />
+          •After completing all the application steps, proceed to pay the application fee.<br />
         </label>
-        <div><button type="submit">Application Fee</button></div>        
+        <div>
+          <button type="button" onClick={handleApplicationFeeClick}>
+            Application Fee
+          </button>
+        </div>
 
-    </div>
+        
+        
+      </div>
     </Sidebar>
   );
-}
+};
 
 export default PersonalStatement;
